8978081ff0a765be453755d962c30f04e23a58ec
Fz Wang <wangfengzhou002@gmail.com>
2022-10-02 12:57:05 +0100

    Update README.m

--- README.m
+++ README.m
@@ -2,9 +2,13 @@ Reference:
 [https://www.geeksforgeeks.org/c-hello-world-program/]
 
 Version Control
+V0.0.1
 
 Format according to Google coding
+[https://google.github.io/styleguide/]
 
-Author
+Author: 
+Peter and Neil
 
 Updated on 
+01-OCT-2022
